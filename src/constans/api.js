b59fec1727cb83f6485e7b83d76f55eb68fb48ad
Oleksandr Kostiushko <oleksandr.kostiushko@oa.edu.ua>
2024-08-21 16:49:55 +0300

    Update api.js

--- src/constans/api.js
+++ src/constans/api.js
@@ -10,7 +10,6 @@ export const API_PEOPLE = HTTPS + SWAPI_ROOT + SWAPI_PEOPLE + SWAPI_PARAM_PAGE;
 export const API_PERSON = HTTPS + SWAPI_ROOT + SWAPI_PEOPLE;
 
 //vusialguide
-
 const GUIDE_ROOT_IMG = 'https://starwars-visualguide.com/assets/img/';
 const GUIDE_PEOPLE = 'characters';
 export const GUIDE_IMG_EXTENSION = '.jpg';
